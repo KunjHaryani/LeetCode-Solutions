166007f7a384b1581d9dc240c93b74c833cd2f05
KunjHaryani <0712kunjharyani@gmail.com>
2026-07-02 11:35:33 +0530

    Added Solution to problem 704 Binary Search

---
+++ Binary Search/704_Binary_Search.cpp
@@ -0,0 +1,22 @@
+// LeetCode 704 - Binary Search
+// Link - https://leetcode.com/problems/binary-search/
+// Topic - Binary Search 
+// Time Complexity - O(log n)
+// Space Complexity - O(1)
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       int n=nums.size();
+       int st=0;
+       int end=n-1;
+       while(st<=end){
+        int mid = st + (end-st)/2;
+        if(nums[mid]==target) return mid;
+        else if(nums[mid]>target){
+            end=mid-1;
+        }
+        else st=mid+1;
+       }
+       return -1; 
+    }
+};
