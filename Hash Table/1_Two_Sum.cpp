c3c606164e83a05e5e1c3896c6a17ba937941b6b
KunjHaryani <0712kunjharyani@gmail.com>
2026-07-11 21:54:22 +0530

    Added Solution Of Q1

--- Hash Table/1_Two_Sum.cpp
+++ Hash Table/1_Two_Sum.cpp
@@ -8,7 +8,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
         unordered_map<int, int> m;
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             int first = nums[i];
             int sec = target - first;
             if (m.find(sec) != m.end()) {
