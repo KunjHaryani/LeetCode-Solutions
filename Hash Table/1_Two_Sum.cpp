bdeede14fb19a8dd34e06db353cc50429ce949cf
KunjHaryani <0712kunjharyani@gmail.com>
2026-07-11 21:52:48 +0530

    Added Hash Table Folder

---
+++ Hash Table/1_Two_Sum.cpp
@@ -0,0 +1,22 @@
+// LeetCode 1 - Two Sum
+// Link - https://leetcode.com/problems/two-sum/
+// Topic - Hash Table 
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int, int> m;
+        for (int i = 0; i < n; i++) {
+            int first = nums[i];
+            int sec = target - first;
+            if (m.find(sec) != m.end()) {
+                return {i, m[sec]};
+            } else {
+                m[first] = i;
+            }
+        }
+        return {};
+    }
+};
